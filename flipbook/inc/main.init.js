7737fc420ca6c2e99c9794988895debf06692426
al370306 <aaron.lemley@gmail.com>
2018-11-06 20:55:40 -0500

    adding all files to new repository
    doing my best to upload this stuff

---
+++ flipbook/inc/main.init.js
@@ -0,0 +1,285 @@
+window.flipbookSettings = {
+    "settings": {
+        "DownloadButtonEnabled": true,
+        "LinksTooltipEnabled": true,
+        "PageWidth": 1400,
+        "PageHeight": 2100,
+        "MobileImageMaxWidth": 900,
+        "MobileImageMaxHeight": 1350,
+        "ThumbnailWidth": 93,
+        "ThumbnailHeight": 140,
+        "DownloadUrl": "inc\/pdf\/flipbook.pdf"
+    },
+    "pages": {
+        "1": {
+            "links": [],
+            "videos": []
+        },
+        "2": {
+            "links": [],
+            "videos": []
+        },
+        "3": {
+            "links": [
+                {
+                    "left": 423.54375,
+                    "top": 1318.0013602817,
+                    "width": 776.38539416667,
+                    "height": 63.090644166667,
+                    "link": "mailto:comms@potomacgreenshoa.com",
+                    "is_internal": false
+                }
+            ],
+            "videos": []
+        },
+        "4": {
+            "links": [
+                {
+                    "left": 234.55509259233,
+                    "top": 821.15444444483,
+                    "width": 456.46189814867,
+                    "height": 48.069907407433,
+                    "link": "mailto:cberuete@sentrymgt.com",
+                    "is_internal": false
+                }
+            ],
+            "videos": []
+        },
+        "5": {
+            "links": [
+                {
+                    "left": 125.89208333333,
+                    "top": 326.48615740767,
+                    "width": 761.15731481533,
+                    "height": 44.372222222183,
+                    "link": "http:\/\/potomacgreenshoa.sentrywebsites.com\/",
+                    "is_internal": false
+                },
+                {
+                    "left": 165.83,
+                    "top": 1276.838888885,
+                    "width": 910.40930555517,
+                    "height": 44.372222222183,
+                    "link": "http:\/\/potomacgreenshoa.sentrywebsites.com\/calendar\/",
+                    "is_internal": false
+                }
+            ],
+            "videos": []
+        },
+        "6": {
+            "links": [],
+            "videos": []
+        },
+        "7": {
+            "links": [],
+            "videos": []
+        },
+        "8": {
+            "links": [
+                {
+                    "left": 193.23694444483,
+                    "top": 1060.0650925923,
+                    "width": 443.636990741,
+                    "height": 44.372222222183,
+                    "link": "http:\/\/www.capitalbikeshare.com",
+                    "is_internal": false
+                },
+                {
+                    "left": 772.70893518467,
+                    "top": 1150.8058333333,
+                    "width": 356.17555555517,
+                    "height": 44.372222222183,
+                    "link": "https:\/\/bit.ly\/2HbXLr8",
+                    "is_internal": false
+                },
+                {
+                    "left": 568.93130662667,
+                    "top": 1196.176203705,
+                    "width": 467.82832300283,
+                    "height": 44.372222222183,
+                    "link": "http:\/\/www.visitalexandriava.com",
+                    "is_internal": false
+                }
+            ],
+            "videos": []
+        },
+        "9": {
+            "links": [
+                {
+                    "left": 550.6501388885,
+                    "top": 1000.031018518,
+                    "width": 489.87490740767,
+                    "height": 44.372222222183,
+                    "link": "http:\/\/www.dashbus.com\/schedules",
+                    "is_internal": false
+                },
+                {
+                    "left": 147.8876388885,
+                    "top": 1272.2532407433,
+                    "width": 897.58666666667,
+                    "height": 44.372222222183,
+                    "link": "http:\/\/www.wmata.com\/schedules\/timetables\/upload\/11y.pdf",
+                    "is_internal": false
+                }
+            ],
+            "videos": []
+        },
+        "10": {
+            "links": [
+                {
+                    "left": 298.35231481533,
+                    "top": 629.94134259233,
+                    "width": 366.25166666667,
+                    "height": 44.372222222183,
+                    "link": "http:\/\/www.alexandriava.gov",
+                    "is_internal": false
+                },
+                {
+                    "left": 124.83333333333,
+                    "top": 1107.7561574077,
+                    "width": 600.08277777817,
+                    "height": 44.372222222183,
+                    "link": "https:\/\/request.alexandriava.gov\/CCC",
+                    "is_internal": false
+                }
+            ],
+            "videos": []
+        },
+        "11": {
+            "links": [
+                {
+                    "left": 595.7579701745,
+                    "top": 1807.168304445,
+                    "width": 411.47651163733,
+                    "height": 45.0211995443,
+                    "link": "mailto:cberuete@sentrymgt.com",
+                    "is_internal": false
+                }
+            ],
+            "videos": []
+        },
+        "12": {
+            "links": [],
+            "videos": []
+        },
+        "13": {
+            "links": [
+                {
+                    "left": 278.83106481533,
+                    "top": 455.20060185133,
+                    "width": 466.81055555517,
+                    "height": 44.372222222183,
+                    "link": "https:\/\/tinyurl.com\/ybbgd7vz",
+                    "is_internal": false
+                }
+            ],
+            "videos": []
+        },
+        "14": {
+            "links": [],
+            "videos": []
+        },
+        "15": {
+            "links": [
+                {
+                    "left": 126.50289535567,
+                    "top": 772.14468638067,
+                    "width": 414.80450948067,
+                    "height": 45.0211995443,
+                    "link": "mailto:cberuete@sentrymgt.com",
+                    "is_internal": false
+                }
+            ],
+            "videos": []
+        },
+        "16": {
+            "links": [
+                {
+                    "left": 737.33333333333,
+                    "top": 424.44078703717,
+                    "width": 468.6713888885,
+                    "height": 51.767592592567,
+                    "link": "http:\/\/www.buzzbakeshop.com",
+                    "is_internal": false
+                },
+                {
+                    "left": 737.33333333333,
+                    "top": 586.65703703717,
+                    "width": 470.30472222183,
+                    "height": 51.767592592567,
+                    "link": "http:\/\/www.lenaswoodfire.com",
+                    "is_internal": false
+                },
+                {
+                    "left": 737.33333333333,
+                    "top": 748.87328703717,
+                    "width": 419.7621296295,
+                    "height": 51.767592592567,
+                    "link": "http:\/\/www.lostdogcafe.com",
+                    "is_internal": false
+                },
+                {
+                    "left": 30.333333333333,
+                    "top": 1278.688703705,
+                    "width": 525.924259259,
+                    "height": 51.767592592567,
+                    "link": "http:\/\/www.rusticorestaurant.com",
+                    "is_internal": false
+                },
+                {
+                    "left": 27.0052820842,
+                    "top": 1448.3316687467,
+                    "width": 439.91991760267,
+                    "height": 98.36020326965,
+                    "link": "https:\/\/www.facebook.com\/vasoskitchensince2006",
+                    "is_internal": false
+                },
+                {
+                    "left": 30.333333333333,
+                    "top": 1662.16231482,
+                    "width": 429.02222222183,
+                    "height": 51.767592592567,
+                    "link": "http:\/\/www.bugsyspizza.com",
+                    "is_internal": false
+                },
+                {
+                    "left": 30.333333333333,
+                    "top": 1825.0620370383,
+                    "width": 641.39185185133,
+                    "height": 51.767592592567,
+                    "link": "http:\/\/www.montereyspizzadelivery.com",
+                    "is_internal": false
+                }
+            ],
+            "videos": []
+        },
+        "17": {
+            "links": [],
+            "videos": []
+        },
+        "18": {
+            "links": [],
+            "videos": []
+        }
+    },
+    "content": {
+        "1": "Potomac Greens Potomac Greens Welcome to Welcome to \f",
+        "2": "Copyright \u00a9 2018 Potomac Greens Homeowners Association All rights reserved Acknowledgements: Christie Beruete, Sentry Management Photo Credits: Nicole McCullar, Michelle Roeser, MD Design: Ashley Lemley Created by Potomac Greens HOA Communications Committee Published by Potomac Greens HOA \f",
+        "3": "Hi, Neighbor! We\u2019re glad you have chosen Potomac Greens to be your home. We\u2019ve compiled some information that may come in handy as you settle in. We love our neighborhood and know that you will, too. If you have any questions, please feel free to reach out to comms@potomacgreenshoa.com. \f",
+        "4": "Community Management The Potomac Greens Homeowners Association (PGHOA) is managed by Sentry Management. The Sentry liaison to Potomac Greens is Christie Beruete, who will be able to help you with most neighborhood matters directly or connect you with another appropriate contact: Potomac Greens HOA c\/o Sentry Management 4401 Ford Avenue, Suite 1150 Alexandria, VA 22302 Attn: Christie Beruete, Community Manager Phone 703-642-3246 ext. 56511 Email: cberuete@sentrymgt.com \f",
+        "5": "Website The web address for the Potomac Greens community is http:\/\/potomacgreenshoa.sentrywebsites.com\/. This website is the home of nearly all information that is relevant to PG residents and homeowners, everything from the Metro shuttle schedule to neighborhood announcements to information about exterior paint colors for your home. The website also features a link to the login page for making your monthly homeowners association (HOA) payments. Facebook Our community\u2019s invitation-only Facebook group can be joined by searching Facebook for \u201cPotomac Greens Community\u201d and requesting access. This page is not an official communication tool of our Board of Directors (more info on the Board below), but it is a frequently updated resource for neighborhood news, vendor recommendations, local government updates, and more. Calendar Our calendar of information of community events can be found at http:\/\/potomacgreenshoa.sentrywebsites.com\/calendar\/. \f",
+        "6": "Meetings: Our Board of Directors meetings are usually held at 7:00 p.m. on the fourth Thursday of every month (except in November and December) and as a new resident you are invited and encouraged to attend. Homeowners will be notified of the date and time of each meeting by way of a mailed postcard. There will also be a flyer posted on the community bulletin board outside the Clubhouse, a post on the Potomac Greens website\u2019s homepage, announcements section, and calendar, a Facebook post, and signs placed in front of the Clubhouse and on both sides of the traffic circle at the entrance of the neighborhood. We look forward to seeing you at the next meeting. It\u2019s a great way to meet your new neighbors. President Ron Lafond Vice President Bob Berdelle Treasurer Laura Mondro Secretary Renee Bemish Member-at-Large Mary-Jane Roth Board of Directors \f",
+        "7": "Committees Potomac Greens residents are welcome and encouraged to volunteer for our committees, which are dedicated to shaping our community and supporting the efforts of the Board. Architectural Review Board Reviews and approves changes to the exterior of homes in our neighborhood, with the goal of maintaining the architectural character of Potomac Greens. Finance Committee Assists the Board in fulfilling the Board\u2019s financial management oversight responsibilities. Landscape Committee Reviews, evaluates, and submits to the Board recommendations regarding all landscape issues related to the grounds controlled by the PGHOA. Rules Committee Researches and analyzes necessary protocols and guidelines that may be needed in order for our community to run smoothly and thrive. Social Committee Organizes recreational community events, and provides opportunities to come together and enjoy our lovely neighborhood. Communications Committee Aids the flow of information within the community via email, Facebook page, the neighborhood bulletin board, and website. \f",
+        "8": "Metro Shuttle Service Potomac Greens employs a private shuttle service to transport residents from the Clubhouse to the Braddock Road Metro Station. The shuttle runs every 15 minutes on weekdays 6:00 a.m. - 8:30 a.m. and 4:30 p.m. - 7:00 p.m., with the exception of federal holidays and any other incidences of the federal government\u2019s closure that may be announced from time to time. Bike Potomac Greens has easy access to several local bike trails and our city\u2019s popular bike-sharing program, Capital Bikeshare. Additionally, on many Sunday mornings during spring, summer, and fall, several Potomac Greens residents hold an informal group bike ride, details of which are usually posted one or two days prior on the community Facebook page. For information about Capital Bikeshare access points and memberships visit www.capitalbikeshare.com. For a map of Alexandria bike trails, visit https:\/\/bit.ly\/2HbXLr8 or click on the Things to Do at www .visitalexandriava.com. Transportation \f",
+        "9": "Bus The two bus routes that are most convenient to Potomac Greens residents are Alexandria\u2019s DASH AT5 bus and Washington Metro\u2019s (WMATA\u2019s) 11Y bus. The AT5 travels back and forth between the Braddock Road and Van Dorn Metro Stations, and can easily transport Potomac Greens residents to and from the Braddock Road Station when the shuttle isn\u2019t running. The nearest AT5 stop is at the intersection of Potomac Greens Drive and Slaters Lane. For more information about the AT5 route and schedule, visit www.dashbus.com\/schedules. The 11Y travels back and forth between Mt. Vernon and the District, mostly by way of George Washington Parkway. The nearest 11Y stop is at the intersection of Abingdon Drive and Slaters Lane. For more information about the 11Y route and schedule, visit www.wmata.com\/schedules\/timetables\/upload\/11y.pdf Parking Please note that our streets are public streets, and curb parking throughout Potomac Greens is first come, first served. \f",
+        "10": "Trash & Recycling The City of Alexandria collects PG\u2019s trash and recycling weekly on Thursdays. Trash and recycling bins should be placed outside of your garage no earlier than 5:00 p.m. on Wednesday, and brought back inside on Thursday night. In the event that Monday is a federal holiday, or in the event of inclement weather and city government closure, trash and recycling will be picked up on Friday. If you have any questions please refer to the City of Alexandria website at www.alexandriava.gov or to the city\u2019s customer service system, Call Click Connect. Call Click Connect Alexandria\u2019s online customer service system allows residents to submit service requests, report problems, search for information, or find the right contact to call for various issues and topics of interest. Staff usually respond to requests within five business days. Visit https:\/\/request.alexandriava.gov\/CCC to submit a query. \f",
+        "11": "Mail USPS mailboxes are located throughout Potomac Greens driveways in gray banks of eight-box units. Your mailbox is likely located at one end of the driveway leading to your garage. You should have received a key to the box associated with your Potomac Greens address when you closed on your house. Outgoing stamped mail (envelopes, not packages) can also be sent through these mailboxes, using the slot labeled \u201cOutgoing Mail.\u201d A key with a plastic rectangular key fob in your mailbox indicates that USPS has delivered a small package to you. Use the key to open the large door of the mailbox bank and retrieve your package. The key will automatically lock in the door, and a postal carrier will retrieve it. If you did not receive a mailbox key at your closing, or you\u2019ve lost your key and need a replacement, contact Christie Beruete. \f",
+        "12": "Clubhouse At the center of Potomac Greens is our intimate and beautiful Clubhouse, which includes a gym, a clubroom (available for private rentals), bathrooms, showers, and a pool. To access any of these facilities (with the exception of the clubroom) you must use your key fob. \f",
+        "13": "Clubroom All Potomac Greens residents may reserve the clubroom for private events throughout the year. Residents must fill out the appropriate form at https:\/\/tinyurl.com\/ybbgd7vz and follow all of the rules and regulations regarding the use and care of the clubroom. Please check the calendar on the Potomac Greens website to see if the room is available before requesting a reservation. \f",
+        "14": "Pool The pool is open daily from Memorial Day weekend through Labor Day weekend each year from 10:00 a.m. \u2013 8:00 p.m. A lifeguard supervises the pool, and pool rules are posted at the Clubhouse\u2019s pool entrance. All residents must sign in and indicate their home address. \f",
+        "15": "Gym The gym is available for use from 4:00 a.m. \u2013 12:00 a.m., seven days a week. If you need to report an issue or a recommendation regarding the gym, please contact Christie Beruete at Sentry Management. Key Fobs Replacement and additional key fobs are available for a fee. Please contact Christie Beruete at Sentry Management for more information. \f",
+        "16": "Buzz Bakeshop www.buzzbakeshop.com Lena\u2019s Wood-Fired Pizza & Tap www.lenaswoodfire.com Lost Dog Caf\u00e9 www.lostdogcafe.com Local Resturants Rustico www.rusticorestaurant.com Monterey\u2019s www.montereyspizzadelivery.com Bugsy\u2019s on King Street www.bugsyspizza.com Vaso\u2019s Kitchen www.facebook.com\/ vasoskitchensince2006 \f",
+        "17": "Emergency Contacts Alexandria Police Department 24-hour non-emergency phone: 703-746-4444 3600 Wheeler Ave., Alexandria, VA 22304 Alexandria Fire Department 24-hour non-emergency phone: 703-746-4444 900 2nd St., Alexandria, VA 22314 Inova Alexandria Hospital Non-emergency phone: 703-504-3000 4320 Seminary Rd., Alexandria, VA 22304 This is the hospital nearest to Potomac Greens, and the one an ambulance will be most likely to travel to in an emergency. We hope this information has been helpful. Welcome to your home in Potomac Greens! \f",
+        "18": "\f"
+    }
+}
